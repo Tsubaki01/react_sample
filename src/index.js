9b78705931b292c09a1933ee6021df9bc3ee7e9d
Tsubaki01 <96325598+Tsubaki01@users.noreply.github.com>
2022-06-19 19:08:04 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Workd!</h1>
+      <p>お元気ですか？</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
